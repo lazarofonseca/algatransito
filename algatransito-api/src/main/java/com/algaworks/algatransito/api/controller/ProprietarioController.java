44f1714a896f32f473c7a814ee81cda395f51cf6
lazarofonseca <lazaronobrega3@gmail.com>
2024-01-09 06:32:36 -0300

    Implementando quey methods

--- algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/api/controller/ProprietarioController.java
@@ -1,24 +1,31 @@
 package com.algaworks.algatransito.api.controller;
 
 import com.algaworks.algatransito.domain.model.Proprietario;
+import com.algaworks.algatransito.domain.repository.ProprietarioRepository;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.TypedQuery;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@AllArgsConstructor
 @RestController
 public class ProprietarioController {
 
-    @PersistenceContext
-    private EntityManager manager;
+
+    private ProprietarioRepository proprietarioRepository;
 
     @GetMapping("/proprietarios")
     public List<Proprietario> listar() {
-        return manager.createQuery("from Proprietario", Proprietario.class).getResultList();
+
+        // return proprietarioRepository.findByNomeContaining("João");
+        return proprietarioRepository.findAll();
+
     }
 
 }
