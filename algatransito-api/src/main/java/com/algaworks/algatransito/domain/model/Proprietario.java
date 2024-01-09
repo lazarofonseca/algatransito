4554a521ef95844cbd0b224d6bb1105d6f3674f8
lazarofonseca <lazaronobrega3@gmail.com>
2024-01-09 05:31:41 -0300

    Mapeando entidades com jakarta persistence

--- algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
+++ algatransito-api/src/main/java/com/algaworks/algatransito/domain/model/Proprietario.java
@@ -1,16 +1,25 @@
 package com.algaworks.algatransito.domain.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Proprietario {
 
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String nome;
     private String email;
     private String telefone;
+
+
 
 
 }
